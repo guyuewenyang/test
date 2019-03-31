076e0f10d47694af052b3c6127004f566113e582
hwy15501077597@163.com <hwy15501077597@163.com>
2019-03-31 16:17:46 +0800

    添加一个模型文件

---
+++ test/Mymodel.swift
@@ -0,0 +1,18 @@
+//
+//  Mymodel.swift
+//  test
+//
+//  Created by 胡文阳 on 2019/3/31.
+//  Copyright © 2019 胡文阳. All rights reserved.
+//
+
+import UIKit
+
+class Mymodel: NSObject {
+
+    
+    var name:String?
+    
+    
+    
+}
